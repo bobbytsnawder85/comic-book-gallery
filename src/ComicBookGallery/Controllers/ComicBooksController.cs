492e32ee107a083a541aed097bfc3eb538afb700
Bobby Snawder <bobby.snawder85@outlook.com>
2017-10-22 16:01:35 -0500

    Updated Comic Book Detail view

--- src/ComicBookGallery/Controllers/ComicBooksController.cs
+++ src/ComicBookGallery/Controllers/ComicBooksController.cs
@@ -10,6 +10,19 @@ namespace ComicBookGallery.Controllers
     {
         public ActionResult Detail()
         {
+            ViewBag.SeriesTitle = "The Amazing Spider-Man";
+            ViewBag.IssueNumber = 700;
+            ViewBag.Description = "<p>Final issue! Witness the final hours of Doctor Octopus' life and his one, last, great act of revenge! Even if Spider-Man survives... <strong>will Peter Parker?</strong></p>";
+            ViewBag.Artists = new string[]
+            {
+                "Script: Dan Slott",
+                "Pencils: Humberto Ramos",
+                "Inks: Victor Olazaba",
+                "Colors: Edgar Delgado",
+                "Letters: Chris Eliopoulos"
+            };
+
+
             //ActionResult is almost always the return type for action methods.
             //Controller base class's View() method can be used to return ViewResult objects.
             return View();
